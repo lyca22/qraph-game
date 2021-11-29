7246f7b098d3b7d013e4782c33d781004a45ad1e
Julian Bolanos <julian.camilob24@gmail.com>
2021-11-29 16:54:14 -0500

    Board class added

--- src/model/Board.java
+++ src/model/Board.java
@@ -1,5 +1,34 @@
 package model;
 
+import java.util.ArrayList;
+
+import datastr.Graph;
+
 public class Board {
 
+	private Graph<Box> graph;
+	private ArrayList<Player> players;
+	//private Path image;		PENDING: ADD TO CONSTRUSTOR
+	
+	public Board(Graph<Box> graph, ArrayList<Player> players) {
+		this.graph = graph;
+		this.players = players;
+	}
+
+	public Graph<Box> getGraph() {
+		return graph;
+	}
+
+	public void setGraph(Graph<Box> graph) {
+		this.graph = graph;
+	}
+
+	public ArrayList<Player> getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(ArrayList<Player> players) {
+		this.players = players;
+	}
+	
 }
