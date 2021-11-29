d842705236a09186ce79dbb21fec35aaca4f22fa
Julian Bolanos <julian.camilob24@gmail.com>
2021-11-29 16:26:43 -0500

    Question class added

--- src/model/Question.java
+++ src/model/Question.java
@@ -1,5 +1,41 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Question {
 
+	private String statement;
+	private ArrayList<String> options;
+	private int answer;
+	
+	public Question(String statement, ArrayList<String> options, int answer) {
+		this.statement = statement;
+		this.options = options;
+		this.answer = answer;
+	}
+
+	public String getStatement() {
+		return statement;
+	}
+
+	public void setStatement(String statement) {
+		this.statement = statement;
+	}
+
+	public ArrayList<String> getOptions() {
+		return options;
+	}
+
+	public void setOptions(ArrayList<String> options) {
+		this.options = options;
+	}
+
+	public int getAnswer() {
+		return answer;
+	}
+
+	public void setAnswer(int answer) {
+		this.answer = answer;
+	}
+	
 }
