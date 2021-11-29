6ebd754bad1e6c1b5d3ddfc1322f4ccd434ccf5e
Julian Bolanos <julian.camilob24@gmail.com>
2021-11-29 16:31:37 -0500

    Button identifier enum added

---
+++ src/model/ButtonIdentifier.java
@@ -0,0 +1,17 @@
+package model;
+
+public enum ButtonIdentifier {
+
+	END_GAME,
+	PLAY,
+	START,
+	ADD_PLAYER,
+	CHANGE_AVATAR,
+	DELETE_PLAYER,
+	INCREASE_ROUNDS,
+	DECREASE_ROUNDS,
+	CHANGE_GRAPH,
+	THROW_DICE,
+	EXIT
+	
+}
