48da86cb59b5ef733e2c71b4f0a854e9221a8191
Julian Bolanos <julian.camilob24@gmail.com>
2021-11-29 16:50:35 -0500

    Button class added

--- src/model/Button.java
+++ src/model/Button.java
@@ -1,5 +1,70 @@
 package model;
 
 public class Button {
+	
+	private int posX;
+	private int posY;
+	private int height;
+	private int width;
+	private boolean state;
+	private ButtonIdentifier identifier;
+	//private Path image;		PENDING TO ADD IN CONSTRUCTOR
+	
+	public Button(int posX, int posY, int height, int width, boolean state, ButtonIdentifier identifier) {
+		this.posX = posX;
+		this.posY = posY;
+		this.height = height;
+		this.width = width;
+		this.state = state;
+		this.identifier = identifier;
+	}
 
+	public int getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public boolean isState() {
+		return state;
+	}
+
+	public void setState(boolean state) {
+		this.state = state;
+	}
+
+	public ButtonIdentifier getIdentifier() {
+		return identifier;
+	}
+
+	public void setIdentifier(ButtonIdentifier identifier) {
+		this.identifier = identifier;
+	}
+	
 }
