d2ac025f7c2a5527eb10edd5f5b524df89c0e313
lyca22 <ariep2206@gmail.com>
2021-11-30 20:31:31 -0500

    Created classes for unitary tests

---
+++ unit-testing/model/ControllerTest.java
@@ -0,0 +1,59 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ControllerTest {
+
+	@Test
+	void testStart() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testMovePlayer() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testGetQuestion() {
+		fail("Not yet implemented");
+	}	
+	
+	@Test
+	void testRechargeCoins() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testDuplicateCoins() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testStealCoins() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testStealCrowns() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testTriggerCrownEvent() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testTriggerCrocodileEvent() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testFindWinner() {
+		fail("Not yet implemented");
+	}
+
+}
